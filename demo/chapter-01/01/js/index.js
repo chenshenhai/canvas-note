2ea593f6f6160bc6efcf9fb45b45467a9603428b
chenshenhai <cshenhai@gmail.com>
2019-03-13 23:14:27 +0800

    feat: add chapter-01/01

--- demo/chapter-01/01/js/index.js
+++ demo/chapter-01/01/js/index.js
@@ -11,3 +11,18 @@
   ctx.strokeStyle = '#000';
   ctx.stroke();
 })(document);
+
+
+(function(doc) {
+  const canvas = doc.getElementById('canvas-2');
+  canvas.width = 400;
+  canvas.height = 400;
+  canvas.style = 'border: 1px solid #ccc;';
+
+  const ctx = canvas.getContext('2d');
+  ctx.moveTo(100, 100);
+  ctx.lineTo(300, 100);
+  ctx.lineWidth = 12;
+  ctx.strokeStyle = '#000';
+  ctx.stroke();
+})(document);
