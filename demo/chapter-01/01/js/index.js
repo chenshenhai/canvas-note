80d57c7afad8afa580699a7a8dfe93d6a3d30412
chenshenhai <cshenhai@gmail.com>
2019-03-13 23:10:17 +0800

    feat: add chapter-01/01

--- demo/chapter-01/01/js/index.js
+++ demo/chapter-01/01/js/index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+(function(doc) {
+  const canvas = doc.getElementById('canvas-1');
+  canvas.width = 400;
+  canvas.height = 400;
+  canvas.style = 'border: 1px solid #ccc;';
+
+  const ctx = canvas.getContext('2d');
+  ctx.moveTo(100, 100);
+  ctx.lineTo(300, 100);
+  ctx.lineWidth = 12;
+  ctx.strokeStyle = '#000';
+  ctx.stroke();
+})(document);
