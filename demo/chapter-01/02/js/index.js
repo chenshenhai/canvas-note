172a442956a50f98ed794be387607260402c5dea
chenshenhai <cshenhai@gmail.com>
2019-03-15 21:43:06 +0800

    feat: update chapter-01/02

--- demo/chapter-01/02/js/index.js
+++ demo/chapter-01/02/js/index.js
@@ -1,20 +1,29 @@
 (function(doc) {
   // 绘制矩形 canvas-1
   const canvas = doc.getElementById('canvas-1');
+  canvas.width = 400;
+  canvas.height = 400;
   const context = canvas.getContext('2d');
+  
   context.rect(40, 40, 200, 300);
-  context.lineWidth = 12;
+  context.lineWidth = 10;
   context.strokeStyle = '#000';
   context.stroke();
 })(document);
 
 
 (function(doc) {
-  // 绘制矩形 canvas-1
+  // 绘制圆形 canvas-2
   const canvas = doc.getElementById('canvas-2');
+  canvas.width = 400;
+  canvas.height = 400;
+
   const context = canvas.getContext('2d');
-  context.rect(40, 40, 200, 300);
-  context.lineWidth = 12;
+  context.beginPath();
+  context.arc(200, 200, 100, Math.PI * 0, Math.PI * 2, true)
+  context.lineWidth = 10;
   context.strokeStyle = '#000';
   context.stroke();
+
 })(document);
+
