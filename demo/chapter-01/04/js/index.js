15508d3cd88240206e5dcf5cc58cb6fdb1bbe48d
chenshenhai <cshenhai@gmail.com>
2019-03-17 17:34:46 +0800

    feat: update chapter-01/04

--- demo/chapter-01/04/js/index.js
+++ demo/chapter-01/04/js/index.js
@@ -1,22 +1,101 @@
 (function(doc) {
-  // 绘制填充文本 canvas-1
+  // 绘制文本 canvas-1
   const canvas = doc.getElementById('canvas-1');
   canvas.width = 400;
   canvas.height = 400;
   const context = canvas.getContext('2d');
 
-  context.font = '40px Microsoft YaHei'
-  context.fillText('hello canvas-note!', 50, 200, 300)
+  context.font = '40px Microsoft YaHei';
+
+  // 填充文本
+  context.fillText('hello canvas-note!', 50, 100, 300)
+  // 描边文本
+  context.strokeText('hello canvas-note!', 50, 200, 300)
 })(document);
 
 
 (function(doc) {
-  // 绘制描边文本 canvas-2
+  // 文本对齐方式 canvas-2
   const canvas = doc.getElementById('canvas-2');
   canvas.width = 400;
   canvas.height = 400;
   const context = canvas.getContext('2d');
+  context.font = '20px Microsoft YaHei';
 
-  context.font = '40px Microsoft YaHei'
-  context.strokeText('hello canvas-note!', 50, 200, 300)
+  context.textAlign = 'start';
+  context.fillText('hello canvas-note!', 100, 50, 200);
+
+  context.textAlign = 'end';
+  context.fillText('hello canvas-note!', 100, 100, 200);
+
+  context.textAlign = 'left';
+  context.fillText('hello canvas-note!', 100, 150, 200);
+
+  context.textAlign = 'right';
+  context.fillText('hello canvas-note!', 100, 200, 200);
+
+  context.textAlign = 'center';
+  context.fillText('hello canvas-note!', 100, 250, 200);
+})(document);
+
+
+(function(doc) {
+  // 基线对齐方式 canvas-3
+  const canvas = doc.getElementById('canvas-3');
+  canvas.width = 400;
+  canvas.height = 400;
+  const context = canvas.getContext('2d');
+  context.font = '20px Microsoft YaHei';
+
+  context.textBaseline = 'top';
+  context.fillText('hello canvas-note!', 100, 50, 200);
+
+  context.textBaseline = 'hanging';
+  context.fillText('hello canvas-note!', 100, 100, 200);
+
+  context.textBaseline = 'middle';
+  context.fillText('hello canvas-note!', 100, 150, 200);
+
+  context.textBaseline = 'alphabetic';
+  context.fillText('hello canvas-note!', 100, 200, 200);
+
+  context.textBaseline = 'ideographic';
+  context.fillText('hello canvas-note!', 100, 250, 200);
+
+  context.textBaseline = 'bottom';
+  context.fillText('hello canvas-note!', 100, 300, 200);
+})(document);
+
+
+(function(doc) {
+  // 文本方向方式 canvas-4
+  const canvas = doc.getElementById('canvas-4');
+  canvas.width = 400;
+  canvas.height = 400;
+  const context = canvas.getContext('2d');
+  context.font = '20px Microsoft YaHei';
+
+  context.direction = 'ltr';
+  context.fillText('hello canvas-note!', 100, 150, 200);
+
+  context.direction = 'rtl';
+  context.fillText('hello canvas-note!', 100, 200, 200);
+
+  context.direction = 'inherit';
+  context.fillText('hello canvas-note!', 100, 250, 200);
+})(document);
+
+
+(function(doc) {
+  // 文本宽度预测量 canvas-5
+  const canvas = doc.getElementById('canvas-5');
+  canvas.width = 400;
+  canvas.height = 400;
+  const context = canvas.getContext('2d');
+  context.font = '20px Microsoft YaHei';
+
+  const textContent = 'hello canvas-note!'
+  context.fillText(textContent, 100, 100, 200);
+  const text = context.measureText(textContent);
+  console.log(text.width);
 })(document);
