e739d8431bf01be3514926a7be6b01b5a5d689e1
chenshenhai <cshenhai@gmail.com>
2019-03-19 22:48:19 +0800

    feat: add chapter-02

---
+++ demo/chapter-02/01/js/index.js
@@ -0,0 +1,34 @@
+(function(doc) {
+
+  // 绘制运动 canvas-1
+  const canvas = doc.getElementById('canvas-1');
+  const requestAnimationFrame = window.requestAnimationFrame;
+  canvas.width = 500;
+  canvas.height = 500;
+  const context = canvas.getContext('2d');
+  let moveDistance = 100;
+  let moveTimestamp = 0;
+  let moveDirection = 1;
+
+  function clear() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function draw(timestamp) {
+    if (!(moveTimestamp > 0)) {
+      moveTimestamp = timestamp;
+    }
+    moveDistance = moveDistance + moveDirection * (timestamp - moveTimestamp) / 10;
+    context.fillStyle = 'red';
+    clear();
+    context.fillRect(moveDistance, 200, 50, 50);
+    if (moveDistance > 400 || moveDistance < 100) {
+      moveDirection = 0 - moveDirection;
+    }
+    requestAnimationFrame(draw);
+    moveTimestamp = timestamp;
+  }
+
+  requestAnimationFrame(draw);
+  
+})(document);
