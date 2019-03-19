03fa836a3dbcd47b42044e12d3583fe612215fb4
chenshenhai <cshenhai@gmail.com>
2019-03-19 23:23:05 +0800

    feat: update chpater-02/01

--- demo/chapter-02/01/js/index.js
+++ demo/chapter-02/01/js/index.js
@@ -1,32 +1,33 @@
 (function(doc) {
 
-  // 绘制运动 canvas-1
+  // 绘制移动效果动画 canvas-1
   const canvas = doc.getElementById('canvas-1');
   const requestAnimationFrame = window.requestAnimationFrame;
   canvas.width = 500;
   canvas.height = 500;
   const context = canvas.getContext('2d');
-  let moveDistance = 100;
-  let moveTimestamp = 0;
-  let moveDirection = 1;
 
+  let moveDistance = 100; // 移动距离
+  let moveDirection = 1;  // 移动方向，1为向右，-1为向左
+
+  // 绘制函数
   function draw(timestamp) {
-    if (!(moveTimestamp > 0)) {
-      moveTimestamp = timestamp;
-    }
-    moveDistance = moveDistance + moveDirection * (timestamp - moveTimestamp) / 10;
+    // 约间隔16.67ms, 每次浏览器执行重绘就移动 1px
+    moveDistance = moveDistance + moveDirection * 1;
     context.fillStyle = 'red';
-    // TODO
-    console.log('moveDistance =', moveDistance,' timestamp =', timestamp, ' moveTimestamp = ', moveTimestamp);
+    // 清除画布
     context.clearRect(0, 0, canvas.width, canvas.height);
+    // 重绘图案
     context.fillRect(moveDistance, 200, 50, 50);
+    // 控制距离，如果超出距离范围，就往反方向移动
     if (moveDistance > 400 || moveDistance < 100) {
       moveDirection = 0 - moveDirection;
     }
+    // 重复执行移动
     requestAnimationFrame(draw);
-    moveTimestamp = timestamp;
   }
 
+  // 开始执行移动
   requestAnimationFrame(draw);
   
 })(document);
