fe3840cdf3a0086cea2ef2d0733519402c0b5cb7
chenshenhai <cshenhai@gmail.com>
2019-03-24 11:30:57 +0800

    feat: add chapter-01/06

---
+++ demo/chapter-01/06/js/index.js
@@ -0,0 +1,16 @@
+(function(doc) {
+  // 绘制填充颜色 canvas-1
+  const canvas = doc.getElementById('canvas-1');
+  canvas.width = 400;
+  canvas.height = 400;
+  const context = canvas.getContext('2d');
+
+  
+  context.shadowColor = "blue";
+  context.shadowBlur = 20;
+  context.shadowOffsetX = 20;
+  context.shadowOffsetY = 20;
+
+  context.fillStyle = "#aaa";
+  context.fillRect(50, 50, 100, 100);
+})(document);
