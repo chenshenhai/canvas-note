c1d51205ac5c42e1de6be79e2a22f68ee90e4199
chenshenhai <cshenhai@gmail.com>
2019-05-04 15:49:20 +0800

    feat: update chapter-01-03

--- demo/chapter-01/03/js/index.js
+++ demo/chapter-01/03/js/index.js
@@ -6,8 +6,8 @@
     return circle;
   }
 
-  // 重复绘制方法1 canvas-1
-  const canvas = document.getElementById('canvas-1');
+  // Path2D重复绘制 
+  const canvas = document.getElementById('canvas-path2d');
   canvas.width = 400;
   canvas.height = 400;
   const context = canvas.getContext('2d');
@@ -15,7 +15,7 @@
   const c1 = createCircle(100, 100, 50);
   const c2 = createCircle(200, 100, 30);
   const c3 = createCircle(140, 240, 60);
-  const c4= createCircle(300, 280, 80);
+  const c4 = createCircle(300, 280, 80);
   context.strokeStyle = '#125394';
   context.lineWidth = 10;
   context.stroke(c1);
