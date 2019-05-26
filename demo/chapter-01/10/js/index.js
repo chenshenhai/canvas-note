92eaa5daa333a1d2efbf045b5b81a71492235d54
chenshenhai <cshenhai@gmail.com>
2019-05-26 23:15:32 +0800

    feat: update chapter-01-10

--- demo/chapter-01/10/js/index.js
+++ demo/chapter-01/10/js/index.js
@@ -1,18 +1,41 @@
 (function() {
   const canvasElem = document.getElementById('canvas-1');
   
-
-  function drawImage(canvas, imageUrl) {
+  /**
+   * 
+   * @param {HTMLCanvasElement} canvas 
+   * @param {object} opts
+   * opts.imageSrc {string} 图片链接
+   * opts.scale {number} 百分比
+   */
+  function drawImage(canvas, opts = {}) {
+    const { imageSrc, scale = 100 } = opts;
     const img = new window.Image();
     img.onload = function(){
-      canvas.width = img.width;
-      canvas.height = img.height;
+      canvas.width = img.width * scale / 100;
+      canvas.height = img.height * scale /  100;
       const ctx = canvas.getContext('2d');
       ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
     }
-    img.src = imageUrl;
+    img.src = imageSrc;
   }
 
-  drawImage(canvasElem, './img/github-404.png')
+  drawImage(canvasElem, {
+    imageSrc: './img/github-404.png',
+    scale: 100,
+  });
+
+
+  const btnScaleList = document.querySelectorAll('.J_Btn_Scale');
+  btnScaleList.forEach(function(btn) {
+    btn.addEventListener('click', function(event) {
+      const scalePercent = event.target.getAttribute('data-scale') * 1
+      drawImage(canvasElem, {
+        imageSrc: './img/github-404.png',
+        scale: scalePercent,
+      });
+    });
+  })
+
 
 })();
