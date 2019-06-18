e960a65cd8b5eca87039a74f4d9d60334451f9e6
chenshenhai <cshenhai@gmail.com>
2019-06-18 22:54:50 +0800

    feat: add chaper-03/04

--- demo/chapter-03/04/js/index.js
+++ demo/chapter-03/04/js/index.js
@@ -3,14 +3,14 @@
   const canvas2 = document.getElementById('canvas-2');
   
   /**
-   * 异步渲染图片并且获取图片数据
+   * 获取图片数据 getImageData
    * @param {HTMLCanvasElement} canvas 
    * @param {object} opts
    * opts.imageSrc {string} 图片链接
    */
-  function drawImageAndGetData(canvas, opts) {
-    const { imageSrc,} = opts;
+  function getImageData(imageSrc) {
     const img = new window.Image();
+    const canvas = document.createElement('canvas');
     return new Promise(function(resolve, reject) {
       img.onload = function(){
         const drawWidth = img.width;
@@ -32,7 +32,39 @@
     });
   }
 
-  const imgData = await drawImageAndGetData(canvas1, { imageSrc: './img/github-404.png', });
-  console.log('imgData = ', imgData);
+  function renderImage(canvas, imageData) {
+    const ctx = canvas.getContext('2d');
+    canvas.width = imageData.width;
+    canvas.height = imageData.height;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.putImageData(imageData, 0, 0); 
+  }
+  
+
+  function parseGrayImageData(imageData) {
+    const data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+      const redChannel = data[i + 0];
+      const greenChannel = data[i + 1];
+      const blueChannel = data[i + 2];
+      // const alphaChannel = data[i + 3];
+
+      grayChannel = (redChannel + greenChannel + blueChannel) / 3;
+      data[i + 0] = grayChannel;
+      data[i + 1] = grayChannel;
+      data[i + 2] = grayChannel;
+      data[i + 3] = 255;
+    }
+    imageData.data = data;
+    return imageData;
+  }
+
+  const imgData = await getImageData('./img/github-404.png');
+  renderImage(canvas1, imgData);
+
+  const grayImgData = parseGrayImageData(imgData);
+  console.log('grayImgData = ', grayImgData);
+  renderImage(canvas2, grayImgData);
+  
   
 })();
