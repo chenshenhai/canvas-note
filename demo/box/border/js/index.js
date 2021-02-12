4cdc261dc70d9c9c150f612a5e54992cf802e04a
chenshenhai <cshenhai@gmail.com>
2021-02-12 15:23:28 +0800

    feat: update demo/box/border

--- demo/box/border/js/index.js
+++ demo/box/border/js/index.js
@@ -5,8 +5,10 @@
   const image = new Image();
   const x = 40;
   const y = 40;
-  const radius = 50;
-  const lineWidth = 4;
+  const radius = 30;
+  const lineWidth = 20;
+  const limitWidth = 200;
+  const limitHeight = 200;
   const color = '#666666'
   const drawImage = function(image, width, height, radius) {
     var pattern = context.createPattern(image, "no-repeat");
@@ -19,13 +21,13 @@
     // context.clearRect(0, 0, width, height);
     const cvs = document.createElement('canvas');
     const ctx2d = cvs.getContext('2d');
-    cvs.width = width + x;
-    cvs.height = height + y;
-    ctx2d.drawImage(img, x, y, width, height);
+    cvs.width = limitWidth + x;
+    cvs.height = limitHeight + y;
+    ctx2d.drawImage(img, x, y, limitWidth, limitHeight);
     
-    drawImage(cvs, width, height, radius);
+    drawImage(cvs, limitWidth, limitHeight, radius);
   };
-  image.src = "./../../../image/github-404.png";
+  image.src = "./../../../image/lena.png";
 
 
   function drawBorderRadius (ctx, x, y, w, h, r, pattern) {
@@ -35,15 +37,22 @@
     }
 
 
+    const borderR = r + lineWidth / 2;
+    const addW = lineWidth / 2
     ctx.beginPath();
-    ctx.moveTo(x + r - lineWidth, y - lineWidth);
-    ctx.arcTo(x + w + lineWidth, y - lineWidth, x + w, y + h, r);
-    ctx.arcTo(x + w + lineWidth, y + h + lineWidth, x, y + h, r);
-    ctx.arcTo(x - lineWidth, y + lineWidth + h, x, y, r);
-    ctx.arcTo(x - lineWidth, y - lineWidth, x + w, y, r);
+    ctx.moveTo(x + r, y - addW);
+    ctx.arcTo(x + w + addW, y - addW, x + w + addW, y + h, borderR);
+    ctx.arcTo(x + w + addW, y + h + addW, x, y + h +addW, borderR);
+    ctx.arcTo(x - addW, y + addW + h, x - addW, y, borderR);
+    ctx.arcTo(x - addW, y - addW, x + w, y - addW, borderR);
+    // ctx.closePath();  
+    // ctx.fillStyle = color;
+    // ctx.fill(); 
+    
     ctx.closePath();  
-    ctx.fillStyle = color;
-    ctx.fill();  
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = color;
+    ctx.stroke();
 
     ctx.beginPath();
     ctx.moveTo(x + r, y);
