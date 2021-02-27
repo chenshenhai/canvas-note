22e16b54a0b19607d2a7fef0eabf1aefb96a2d10
chenshenhai <cshenhai@gmail.com>
2021-02-27 16:06:04 +0800

    feat: update paint/draw-brush-line

--- demo/paint/draw-brush-line/js/index.js
+++ demo/paint/draw-brush-line/js/index.js
@@ -1,63 +1,75 @@
 import { Brush } from './brush.js';
+import { Watcher } from './watcher.js';
 
-let isPainting = false;
-let lastPoint = {};
+// let isPainting = false;
+// let lastPoint = {};
+
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 const brush = new Brush(ctx);
-
-let positions = [];
-
-function onStart(e) {
-  const x = e.clientX;
-  const y = e.clientY;
-  isPainting = true;
-  positions = [];
-  lastPoint = {
-    x: x - canvas.offsetLeft,
-    y: y - canvas.offsetTop,
-    t: Date.now(),
-  };
-  positions.push(lastPoint)
-}
-
-function onMove(e) {
-  const x = e.clientX;
-  const y = e.clientY;
-  if (isPainting) {
-    let newPoint = {
-      x: x - canvas.offsetLeft,
-      y: y - canvas.offsetTop,
-      t: Date.now(),
-    };
-    lastPoint = newPoint;
-    positions.push(newPoint);
-    drawBrush(positions);
-  }
-}
-
-function onEnd() {
-  isPainting = false;
-}
-
-canvas.addEventListener('mousedown', onStart);
-canvas.addEventListener('mousemove', onMove);
-canvas.addEventListener('mouseup', onEnd);
-
-const mouseupEvent = new MouseEvent('mouseup');
-document.querySelector('body').addEventListener('mousemove', (e) => {
-  if (e.path[0] !== canvas) {
-    canvas.dispatchEvent(mouseupEvent);
-  }
-}, false)
-
-
-let prevVelocity = 0;
-function drawBrush(positions) {
+const watcher = new Watcher(canvas);
+watcher.onDraw((positions) => {
   if (!(positions.length >= 2)) {
     return;
   }
   const start = positions[positions.length - 2];
   const end = positions[positions.length - 1]
   brush.drawLine(start, end)
-}
+});
+
+
+// let positions = [];
+
+// function onStart(e) {
+//   const x = e.clientX;
+//   const y = e.clientY;
+//   isPainting = true;
+//   positions = [];
+//   lastPoint = {
+//     x: x - canvas.offsetLeft,
+//     y: y - canvas.offsetTop,
+//     t: Date.now(),
+//   };
+//   positions.push(lastPoint)
+// }
+
+// function onMove(e) {
+//   const x = e.clientX;
+//   const y = e.clientY;
+//   if (isPainting) {
+//     let newPoint = {
+//       x: x - canvas.offsetLeft,
+//       y: y - canvas.offsetTop,
+//       t: Date.now(),
+//     };
+//     lastPoint = newPoint;
+//     positions.push(newPoint);
+//     drawBrush(positions);
+//   }
+// }
+
+// function onEnd() {
+//   isPainting = false;
+// }
+
+// canvas.addEventListener('mousedown', onStart);
+// canvas.addEventListener('mousemove', onMove);
+// canvas.addEventListener('mouseup', onEnd);
+
+// const mouseupEvent = new MouseEvent('mouseup');
+// document.querySelector('body').addEventListener('mousemove', (e) => {
+//   if (e.path[0] !== canvas) {
+//     canvas.dispatchEvent(mouseupEvent);
+//   }
+// }, false)
+
+
+// let prevVelocity = 0;
+// function drawBrush(positions) {
+//   if (!(positions.length >= 2)) {
+//     return;
+//   }
+//   const start = positions[positions.length - 2];
+//   const end = positions[positions.length - 1]
+//   brush.drawLine(start, end)
+// }
---
+++ demo/paint/draw-brush-line/js/watcher.js
@@ -0,0 +1,63 @@
+export class Watcher {
+  constructor(canvas) {
+    this._canvas = canvas;
+    this._isPainting = false;
+    this._points = [];
+    this._onDraw = null;
+    this._initEvent();
+  }
+
+  onDraw(callback) {
+    this._onDraw = callback;
+  }
+
+  _initEvent() {
+    const canvas = this._canvas;
+    canvas.addEventListener('mousedown', this._onStart.bind(this));
+    canvas.addEventListener('mousemove', this._onMove.bind(this));
+    canvas.addEventListener('mouseup', this._onEnd.bind(this));
+
+    const mouseupEvent = new MouseEvent('mouseup');
+    document.querySelector('body').addEventListener('mousemove', (e) => {
+      if (e.path[0] !== canvas) {
+        canvas.dispatchEvent(mouseupEvent);
+      }
+    }, false)
+  }
+
+  _onStart(e) {
+    const x = e.clientX;
+    const y = e.clientY;
+    this._isPainting = true;
+    this._points = [];
+    this._lastPoint = {
+      x: x - canvas.offsetLeft,
+      y: y - canvas.offsetTop,
+      t: Date.now(),
+    };
+    this._points.push(this._lastPoint)
+  }
+  
+  _onMove(e) {
+    const x = e.clientX;
+    const y = e.clientY;
+    if (this._isPainting) {
+      let newPoint = { 
+        x: x - canvas.offsetLeft,
+        y: y - canvas.offsetTop,
+        t: Date.now(),
+      };
+      this._lastPoint = newPoint;
+      this._points.push(newPoint);
+
+      if (typeof this._onDraw === 'function') {
+        this._onDraw(this._points);
+      }
+    }
+  }
+  
+  _onEnd() {
+    this._isPainting = false;
+  }
+  
+}
